9445744c4e1e1e79b91fdfa13c298f4a3b530f20
CJ <kubachod@gmail.com>
2023-11-17 12:12:31 +0100

    Initial Commit

---
+++ Rekrut/RekrutApp.swift
@@ -0,0 +1,17 @@
+//
+//  RekrutApp.swift
+//  Rekrut
+//
+//  Created by Jakub Chodara on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RekrutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
